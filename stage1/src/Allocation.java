7d40ce91ef1fee44deaf70543f4e34d92bc52a49
id95 <id95@kent.ac.uk>
2020-01-21 10:54:18 +0000

    Task 1.1

--- stage1/src/Allocation.java
+++ stage1/src/Allocation.java
@@ -1,19 +1,44 @@
-
+import java.awt.*;
+import java.util.Arrays;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
 /**
  * Write a description of class Allocation here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Allocation
-
-    //TODO decide a proper data-structure to store the allocation
-
+public class Allocation    
 {
+   ArrayList<ArrayList<String>> carInfo;
+    
     public Allocation(String allocationFileName, WorldAndRides worldAndRides) throws FileFormatException {
-        //TODO read an allocation from allocationFileName and stores the content in
-        //an appropriate datastructure inside this class
+        
+        carInfo = readFile(allocationFileName);
     }
     
-    //TODO define appropriate methods for this class (e.g. accessor methods)
+    public String[][] readFile(String filename)
+    {
+        int y = 0;
+        ArrayList<ArrayList<String>> temp = new ArrayList<ArrayList<String>>();
+        try{
+            Scanner sc = new Scanner(new BufferedReader(new FileReader(filename)));
+            while(sc.nextLine().split("") != null){
+                String[] line = sc.nextLine().split("");
+                for (String s: line){
+                    int x = 0;
+                    temp[x][y] = line[x];
+                    
+                }
+            }
+        }
+        catch(IOException ex){
+            return null;
+        }
+    }
 }
